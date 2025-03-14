850d668e8dce8c61ab75f82e69ee6898f8312f82
Thomas Solecki <thomasjsolecki@gmail.com>
2025-03-13 21:23:39 -0700

    Add ability to show and hide hard-coded chat message

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+	let open = $state(true);
+</script>
+
+<button class="border p-2 hover:cursor-pointer" onclick={() => (open = !open)}>toggle open</button>
+
+{#if open}
+	<div
+		class="fixed right-8 bottom-8 w-sm bg-slate-800 p-4"
+		transition:fly={{ x: 200, duration: 500 }}
+	>
+		<span class="text-green-500">based_chatter</span>
+		<span class="text-white">
+			glizzy hands glizzy hands glizzy hands glizzy hands glizzy hands glizzy hands glizzy hands
+			glizzy hands glizzy hands glizzy hands glizzy hands glizzy hands
+		</span>
+	</div>
+{/if}
