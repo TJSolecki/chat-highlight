9ccbc704f64a25b6586490e0aace8e80b57de0d2
Thomas Solecki <thomasjsolecki@gmail.com>
2025-03-21 19:03:46 -0700

    Add ability for clicked chats to pop up on main view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,41 @@
 <script lang="ts">
-    import { fly } from "svelte/transition";
-    let open = $state(true);
+    import { fly, fade } from "svelte/transition";
+    import { getColor, type Chat } from "./chat/[username]/+page.svelte";
+    import swoosh from "../assets/swoosh.mp3";
+
+    let chat = $state<Chat>();
+    let timeout = $state<number>();
+    const swooshAudio = new Audio(swoosh);
 
     $effect(() => {
         const listener = () => {
-            open = localStorage.getItem("open") === "true";
+            const chatJson = localStorage.getItem("highlightedChat");
+            if (chatJson) {
+                swooshAudio.play();
+                chat = JSON.parse(chatJson);
+                if (timeout) {
+                    clearTimeout(timeout);
+                }
+                timeout = setTimeout(() => (chat = undefined), 10_000);
+            } else {
+                chat = undefined;
+            }
         };
         window.addEventListener("storage", listener);
         return () => {
             removeEventListener("storage", listener);
         };
     });
-
-    function openPopup() {
-        window.open("/chat", "_blank");
-    }
-
-    function setOpen(value: boolean): void {
-        open = value;
-        localStorage.setItem("open", String(value));
-    }
 </script>
 
-<button class="border p-2 hover:cursor-pointer" onclick={openPopup}>open popup</button>
-<button class="border p-2 hover:cursor-pointer" onclick={() => setOpen(!open)}>toggle open</button>
-
-{#if open}
-    <div class="fixed right-8 bottom-8 w-sm bg-slate-800 p-4" transition:fly={{ x: 200, duration: 500 }}>
-        <span class="text-green-500">based_chatter</span>
-        <span class="text-white">
-            glizzy hands glizzy hands glizzy hands glizzy hands glizzy hands glizzy hands glizzy hands glizzy hands
-            glizzy hands glizzy hands glizzy hands glizzy hands
-        </span>
+{#if chat}
+    <div
+        class="fixed right-8 bottom-8 max-w-sm bg-zinc-900 p-4"
+        in:fly={{ x: 200, duration: 500 }}
+        out:fade={{ duration: 200 }}
+    >
+        <div id={chat.id} class="text-[0.825rem] font-semibold text-white">
+            <span style="color: {getColor(chat)}">{chat.username}</span>: {chat.chat}
+        </div>
     </div>
 {/if}
