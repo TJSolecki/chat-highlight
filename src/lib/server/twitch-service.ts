a894ecdf8ed3a315e89aa05df0c6fb195f427104
Thomas Solecki <thomasjsolecki@gmail.com>
2025-04-01 07:22:48 -0700

    Refactor #request method out of getUsers in TwitchService

--- src/lib/server/twitch-service.ts
+++ src/lib/server/twitch-service.ts
@@ -19,12 +19,16 @@ class TwitchService {
     constructor() {}
 
     async getUsers(channelName: string, { retries = 1, accessToken }: Options): Promise<UserData[]> {
+        const url = `https://api.twitch.tv/helix/users?login=${channelName}`;
+        return (await this.#request<{ data: UserData[] }>(url, { retries, accessToken })).data;
+    }
+
+    async #request<T>(url: string, { accessToken, retries = 1 }: Options): Promise<T> {
         if (accessToken) {
             this.#accessToken = accessToken;
         } else if (!this.#accessToken) {
             this.#accessToken = await getAccessToken();
         }
-        const url = `https://api.twitch.tv/helix/users?login=${channelName}`;
         const response = await fetch(url, {
             headers: {
                 "Client-ID": CLIENT_ID,
@@ -32,11 +36,11 @@ class TwitchService {
             },
         });
         if (response.ok) {
-            return (await response.json()).data;
+            return await response.json();
         } else {
             if (response.status === 401 && retries > 0) {
                 this.#accessToken = await getAccessToken();
-                return await this.getUsers(channelName, { retries: retries - 1 });
+                return await this.#request(url, { retries: retries - 1 });
             }
             console.error(response);
             throw new Error(await response.text());
